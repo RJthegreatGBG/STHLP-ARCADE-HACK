6a93f499e1c17343d57db5a48e857283558563ba
RJthegreatGBG <yasnelimiami1@yahoo.com>
2024-09-01 13:06:48 -0400

    Sonic Walk

--- _maps/Sonic - Dynamic Gfx Script.asm
+++ _maps/Sonic - Dynamic Gfx Script.asm
@@ -1,604 +1,599 @@
-DPLC_32c3: mappingsTable
-	mappingsTableEntry.w	DPLC_32c3_0
-	mappingsTableEntry.w	DPLC_32c3_1
-	mappingsTableEntry.w	DPLC_32c3_2
-	mappingsTableEntry.w	DPLC_32c3_3
-	mappingsTableEntry.w	DPLC_32c3_4
-	mappingsTableEntry.w	DPLC_32c3_5
-	mappingsTableEntry.w	DPLC_32c3_6
-	mappingsTableEntry.w	DPLC_32c3_7
-	mappingsTableEntry.w	DPLC_32c3_8
-	mappingsTableEntry.w	DPLC_32c3_9
-	mappingsTableEntry.w	DPLC_32c3_10
-	mappingsTableEntry.w	DPLC_32c3_11
-	mappingsTableEntry.w	DPLC_32c3_12
-	mappingsTableEntry.w	DPLC_32c3_13
-	mappingsTableEntry.w	DPLC_32c3_14
-	mappingsTableEntry.w	DPLC_32c3_15
-	mappingsTableEntry.w	DPLC_32c3_16
-	mappingsTableEntry.w	DPLC_32c3_17
-	mappingsTableEntry.w	DPLC_32c3_18
-	mappingsTableEntry.w	DPLC_32c3_19
-	mappingsTableEntry.w	DPLC_32c3_20
-	mappingsTableEntry.w	DPLC_32c3_21
-	mappingsTableEntry.w	DPLC_32c3_22
-	mappingsTableEntry.w	DPLC_32c3_23
-	mappingsTableEntry.w	DPLC_32c3_24
-	mappingsTableEntry.w	DPLC_32c3_25
-	mappingsTableEntry.w	DPLC_32c3_26
-	mappingsTableEntry.w	DPLC_32c3_27
-	mappingsTableEntry.w	DPLC_32c3_28
-	mappingsTableEntry.w	DPLC_32c3_29
-	mappingsTableEntry.w	DPLC_32c3_30
-	mappingsTableEntry.w	DPLC_32c3_31
-	mappingsTableEntry.w	DPLC_32c3_32
-	mappingsTableEntry.w	DPLC_32c3_33
-	mappingsTableEntry.w	DPLC_32c3_34
-	mappingsTableEntry.w	DPLC_32c3_35
-	mappingsTableEntry.w	DPLC_32c3_36
-	mappingsTableEntry.w	DPLC_32c3_37
-	mappingsTableEntry.w	DPLC_32c3_38
-	mappingsTableEntry.w	DPLC_32c3_39
-	mappingsTableEntry.w	DPLC_32c3_40
-	mappingsTableEntry.w	DPLC_32c3_41
-	mappingsTableEntry.w	DPLC_32c3_42
-	mappingsTableEntry.w	DPLC_32c3_43
-	mappingsTableEntry.w	DPLC_32c3_44
-	mappingsTableEntry.w	DPLC_32c3_45
-	mappingsTableEntry.w	DPLC_32c3_46
-	mappingsTableEntry.w	DPLC_32c3_47
-	mappingsTableEntry.w	DPLC_32c3_48
-	mappingsTableEntry.w	DPLC_32c3_49
-	mappingsTableEntry.w	DPLC_32c3_50
-	mappingsTableEntry.w	DPLC_32c3_51
-	mappingsTableEntry.w	DPLC_32c3_52
-	mappingsTableEntry.w	DPLC_32c3_53
-	mappingsTableEntry.w	DPLC_32c3_54
-	mappingsTableEntry.w	DPLC_32c3_55
-	mappingsTableEntry.w	DPLC_32c3_56
-	mappingsTableEntry.w	DPLC_32c3_57
-	mappingsTableEntry.w	DPLC_32c3_58
-	mappingsTableEntry.w	DPLC_32c3_59
-	mappingsTableEntry.w	DPLC_32c3_60
-	mappingsTableEntry.w	DPLC_32c3_61
-	mappingsTableEntry.w	DPLC_32c3_62
-	mappingsTableEntry.w	DPLC_32c3_63
-	mappingsTableEntry.w	DPLC_32c3_64
-	mappingsTableEntry.w	DPLC_32c3_65
-	mappingsTableEntry.w	DPLC_32c3_66
-	mappingsTableEntry.w	DPLC_32c3_67
-	mappingsTableEntry.w	DPLC_32c3_68
-	mappingsTableEntry.w	DPLC_32c3_69
-	mappingsTableEntry.w	DPLC_32c3_70
-	mappingsTableEntry.w	DPLC_32c3_71
-	mappingsTableEntry.w	DPLC_32c3_72
-	mappingsTableEntry.w	DPLC_32c3_73
-	mappingsTableEntry.w	DPLC_32c3_74
-	mappingsTableEntry.w	DPLC_32c3_75
-	mappingsTableEntry.w	DPLC_32c3_76
-	mappingsTableEntry.w	DPLC_32c3_77
-	mappingsTableEntry.w	DPLC_32c3_78
-	mappingsTableEntry.w	DPLC_32c3_79
-	mappingsTableEntry.w	DPLC_32c3_80
-	mappingsTableEntry.w	DPLC_32c3_81
-	mappingsTableEntry.w	DPLC_32c3_82
-	mappingsTableEntry.w	DPLC_32c3_83
-	mappingsTableEntry.w	DPLC_32c3_84
-	mappingsTableEntry.w	DPLC_32c3_85
-	mappingsTableEntry.w	DPLC_32c3_86
-	mappingsTableEntry.w	DPLC_32c3_87
+DPLC_e16e: mappingsTable
+	mappingsTableEntry.w	DPLC_e16e_0
+	mappingsTableEntry.w	DPLC_e16e_1
+	mappingsTableEntry.w	DPLC_e16e_2
+	mappingsTableEntry.w	DPLC_e16e_3
+	mappingsTableEntry.w	DPLC_e16e_4
+	mappingsTableEntry.w	DPLC_e16e_5
+	mappingsTableEntry.w	DPLC_e16e_6
+	mappingsTableEntry.w	DPLC_e16e_7
+	mappingsTableEntry.w	DPLC_e16e_8
+	mappingsTableEntry.w	DPLC_e16e_9
+	mappingsTableEntry.w	DPLC_e16e_10
+	mappingsTableEntry.w	DPLC_e16e_11
+	mappingsTableEntry.w	DPLC_e16e_12
+	mappingsTableEntry.w	DPLC_e16e_13
+	mappingsTableEntry.w	DPLC_e16e_14
+	mappingsTableEntry.w	DPLC_e16e_15
+	mappingsTableEntry.w	DPLC_e16e_16
+	mappingsTableEntry.w	DPLC_e16e_17
+	mappingsTableEntry.w	DPLC_e16e_18
+	mappingsTableEntry.w	DPLC_e16e_19
+	mappingsTableEntry.w	DPLC_e16e_20
+	mappingsTableEntry.w	DPLC_e16e_21
+	mappingsTableEntry.w	DPLC_e16e_22
+	mappingsTableEntry.w	DPLC_e16e_23
+	mappingsTableEntry.w	DPLC_e16e_24
+	mappingsTableEntry.w	DPLC_e16e_25
+	mappingsTableEntry.w	DPLC_e16e_26
+	mappingsTableEntry.w	DPLC_e16e_27
+	mappingsTableEntry.w	DPLC_e16e_28
+	mappingsTableEntry.w	DPLC_e16e_29
+	mappingsTableEntry.w	DPLC_e16e_30
+	mappingsTableEntry.w	DPLC_e16e_31
+	mappingsTableEntry.w	DPLC_e16e_32
+	mappingsTableEntry.w	DPLC_e16e_33
+	mappingsTableEntry.w	DPLC_e16e_34
+	mappingsTableEntry.w	DPLC_e16e_35
+	mappingsTableEntry.w	DPLC_e16e_36
+	mappingsTableEntry.w	DPLC_e16e_37
+	mappingsTableEntry.w	DPLC_e16e_38
+	mappingsTableEntry.w	DPLC_e16e_39
+	mappingsTableEntry.w	DPLC_e16e_40
+	mappingsTableEntry.w	DPLC_e16e_41
+	mappingsTableEntry.w	DPLC_e16e_42
+	mappingsTableEntry.w	DPLC_e16e_43
+	mappingsTableEntry.w	DPLC_e16e_44
+	mappingsTableEntry.w	DPLC_e16e_45
+	mappingsTableEntry.w	DPLC_e16e_46
+	mappingsTableEntry.w	DPLC_e16e_47
+	mappingsTableEntry.w	DPLC_e16e_48
+	mappingsTableEntry.w	DPLC_e16e_49
+	mappingsTableEntry.w	DPLC_e16e_50
+	mappingsTableEntry.w	DPLC_e16e_51
+	mappingsTableEntry.w	DPLC_e16e_52
+	mappingsTableEntry.w	DPLC_e16e_53
+	mappingsTableEntry.w	DPLC_e16e_54
+	mappingsTableEntry.w	DPLC_e16e_55
+	mappingsTableEntry.w	DPLC_e16e_56
+	mappingsTableEntry.w	DPLC_e16e_57
+	mappingsTableEntry.w	DPLC_e16e_58
+	mappingsTableEntry.w	DPLC_e16e_59
+	mappingsTableEntry.w	DPLC_e16e_60
+	mappingsTableEntry.w	DPLC_e16e_61
+	mappingsTableEntry.w	DPLC_e16e_62
+	mappingsTableEntry.w	DPLC_e16e_63
+	mappingsTableEntry.w	DPLC_e16e_64
+	mappingsTableEntry.w	DPLC_e16e_65
+	mappingsTableEntry.w	DPLC_e16e_66
+	mappingsTableEntry.w	DPLC_e16e_67
+	mappingsTableEntry.w	DPLC_e16e_68
+	mappingsTableEntry.w	DPLC_e16e_69
+	mappingsTableEntry.w	DPLC_e16e_70
+	mappingsTableEntry.w	DPLC_e16e_71
+	mappingsTableEntry.w	DPLC_e16e_72
+	mappingsTableEntry.w	DPLC_e16e_73
+	mappingsTableEntry.w	DPLC_e16e_74
+	mappingsTableEntry.w	DPLC_e16e_75
+	mappingsTableEntry.w	DPLC_e16e_76
+	mappingsTableEntry.w	DPLC_e16e_77
+	mappingsTableEntry.w	DPLC_e16e_78
+	mappingsTableEntry.w	DPLC_e16e_79
+	mappingsTableEntry.w	DPLC_e16e_80
+	mappingsTableEntry.w	DPLC_e16e_81
+	mappingsTableEntry.w	DPLC_e16e_82
+	mappingsTableEntry.w	DPLC_e16e_83
+	mappingsTableEntry.w	DPLC_e16e_84
+	mappingsTableEntry.w	DPLC_e16e_85
+	mappingsTableEntry.w	DPLC_e16e_86
+	mappingsTableEntry.w	DPLC_e16e_87
 
-DPLC_32c3_0:	dplcHeader
-DPLC_32c3_0_End
+DPLC_e16e_0:	dplcHeader
+DPLC_e16e_0_End
 
-DPLC_32c3_1:	dplcHeader
+DPLC_e16e_1:	dplcHeader
  dplcEntry $10, 0
  dplcEntry 6, $10
-DPLC_32c3_1_End
+DPLC_e16e_1_End
 
-DPLC_32c3_2:	dplcHeader
+DPLC_e16e_2:	dplcHeader
  dplcEntry $10, $16
  dplcEntry 5, $26
-DPLC_32c3_2_End
+DPLC_e16e_2_End
 
-DPLC_32c3_3:	dplcHeader
+DPLC_e16e_3:	dplcHeader
  dplcEntry $10, $2B
  dplcEntry 5, $3B
-DPLC_32c3_3_End
+DPLC_e16e_3_End
 
-DPLC_32c3_4:	dplcHeader
+DPLC_e16e_4:	dplcHeader
  dplcEntry $10, $40
  dplcEntry 5, $50
-DPLC_32c3_4_End
+DPLC_e16e_4_End
 
-DPLC_32c3_5:	dplcHeader
+DPLC_e16e_5:	dplcHeader
  dplcEntry $10, $55
  dplcEntry 5, $65
-DPLC_32c3_5_End
+DPLC_e16e_5_End
 
-DPLC_32c3_6:	dplcHeader
- dplcEntry 8, $6A
- dplcEntry 6, $72
- dplcEntry 6, $78
- dplcEntry 2, $7E
-DPLC_32c3_6_End
+DPLC_e16e_6:	dplcHeader
+ dplcEntry $10, $5CC
+ dplcEntry 9, $5DC
+DPLC_e16e_6_End
 
-DPLC_32c3_7:	dplcHeader
- dplcEntry 8, $6A
- dplcEntry $C, $80
-DPLC_32c3_7_End
+DPLC_e16e_7:	dplcHeader
+ dplcEntry $10, $5E5
+ dplcEntry 8, $5F5
+DPLC_e16e_7_End
 
-DPLC_32c3_8:	dplcHeader
- dplcEntry 6, $8C
- dplcEntry 9, $92
-DPLC_32c3_8_End
+DPLC_e16e_8:	dplcHeader
+ dplcEntry $10, $575
+ dplcEntry 4, $585
+DPLC_e16e_8_End
 
-DPLC_32c3_9:	dplcHeader
- dplcEntry 6, $8C
- dplcEntry 6, $9B
- dplcEntry 6, $A1
- dplcEntry 2, $A7
-DPLC_32c3_9_End
+DPLC_e16e_9:	dplcHeader
+ dplcEntry $10, $589
+ dplcEntry 9, $599
+DPLC_e16e_9_End
 
-DPLC_32c3_10:	dplcHeader
- dplcEntry 6, $8C
- dplcEntry $C, $A9
-DPLC_32c3_10_End
+DPLC_e16e_10:	dplcHeader
+ dplcEntry $10, $5A2
+ dplcEntry 6, $5B2
+DPLC_e16e_10_End
 
-DPLC_32c3_11:	dplcHeader
- dplcEntry 8, $6A
- dplcEntry 4, $B5
- dplcEntry 6, $B9
-DPLC_32c3_11_End
+DPLC_e16e_11:	dplcHeader
+ dplcEntry $10, $5B8
+ dplcEntry 4, $5C8
+DPLC_e16e_11_End
 
-DPLC_32c3_12:	dplcHeader
+DPLC_e16e_12:	dplcHeader
  dplcEntry 6, $BF
  dplcEntry 6, $C5
  dplcEntry 3, $CB
  dplcEntry 6, $CE
  dplcEntry 1, $D4
-DPLC_32c3_12_End
+DPLC_e16e_12_End
 
-DPLC_32c3_13:	dplcHeader
+DPLC_e16e_13:	dplcHeader
  dplcEntry 6, $BF
  dplcEntry 2, $D5
  dplcEntry 4, $D7
  dplcEntry 6, $DB
  dplcEntry 4, $E1
  dplcEntry 1, $E5
-DPLC_32c3_13_End
+DPLC_e16e_13_End
 
-DPLC_32c3_14:	dplcHeader
+DPLC_e16e_14:	dplcHeader
  dplcEntry 6, $E6
  dplcEntry 2, $EC
  dplcEntry 8, $EE
  dplcEntry 3, $F6
-DPLC_32c3_14_End
+DPLC_e16e_14_End
 
-DPLC_32c3_15:	dplcHeader
+DPLC_e16e_15:	dplcHeader
  dplcEntry 6, $F9
  dplcEntry 4, $FF
  dplcEntry 8, $103
  dplcEntry 3, $10B
  dplcEntry 2, $10E
-DPLC_32c3_15_End
+DPLC_e16e_15_End
 
-DPLC_32c3_16:	dplcHeader
+DPLC_e16e_16:	dplcHeader
  dplcEntry 6, $F9
  dplcEntry 2, $110
  dplcEntry 8, $112
  dplcEntry 3, $11A
-DPLC_32c3_16_End
+DPLC_e16e_16_End
 
-DPLC_32c3_17:	dplcHeader
+DPLC_e16e_17:	dplcHeader
  dplcEntry 6, $BF
  dplcEntry 2, $D5
  dplcEntry 1, $CB
  dplcEntry 8, $11D
  dplcEntry 3, $125
-DPLC_32c3_17_End
+DPLC_e16e_17_End
 
-DPLC_32c3_18:	dplcHeader
+DPLC_e16e_18:	dplcHeader
  dplcEntry 8, $128
  dplcEntry 6, $130
  dplcEntry 2, $136
  dplcEntry 6, $138
-DPLC_32c3_18_End
+DPLC_e16e_18_End
 
-DPLC_32c3_19:	dplcHeader
+DPLC_e16e_19:	dplcHeader
  dplcEntry 8, $128
  dplcEntry $C, $13E
-DPLC_32c3_19_End
+DPLC_e16e_19_End
 
-DPLC_32c3_20:	dplcHeader
+DPLC_e16e_20:	dplcHeader
  dplcEntry 6, $14A
  dplcEntry 9, $150
-DPLC_32c3_20_End
+DPLC_e16e_20_End
 
-DPLC_32c3_21:	dplcHeader
+DPLC_e16e_21:	dplcHeader
  dplcEntry 6, $14A
  dplcEntry 6, $159
  dplcEntry 2, $15F
  dplcEntry 6, $161
-DPLC_32c3_21_End
+DPLC_e16e_21_End
 
-DPLC_32c3_22:	dplcHeader
+DPLC_e16e_22:	dplcHeader
  dplcEntry 6, $14A
  dplcEntry $C, $167
-DPLC_32c3_22_End
+DPLC_e16e_22_End
 
-DPLC_32c3_23:	dplcHeader
+DPLC_e16e_23:	dplcHeader
  dplcEntry 8, $128
  dplcEntry 1, $13E
  dplcEntry 9, $173
-DPLC_32c3_23_End
+DPLC_e16e_23_End
 
-DPLC_32c3_24:	dplcHeader
+DPLC_e16e_24:	dplcHeader
  dplcEntry 6, $17C
  dplcEntry 2, $182
  dplcEntry 2, $184
  dplcEntry 9, $186
  dplcEntry 1, $18F
  dplcEntry 1, $190
-DPLC_32c3_24_End
+DPLC_e16e_24_End
 
-DPLC_32c3_25:	dplcHeader
+DPLC_e16e_25:	dplcHeader
  dplcEntry 6, $17C
  dplcEntry 3, $191
  dplcEntry 2, $194
  dplcEntry 2, $196
  dplcEntry 9, $198
  dplcEntry 1, $18F
-DPLC_32c3_25_End
+DPLC_e16e_25_End
 
-DPLC_32c3_26:	dplcHeader
+DPLC_e16e_26:	dplcHeader
  dplcEntry 6, $1A1
  dplcEntry 2, $1A7
  dplcEntry 9, $1A9
  dplcEntry 2, $1B2
-DPLC_32c3_26_End
+DPLC_e16e_26_End
 
-DPLC_32c3_27:	dplcHeader
+DPLC_e16e_27:	dplcHeader
  dplcEntry 6, $1B4
  dplcEntry 3, $1BA
  dplcEntry 2, $1BD
  dplcEntry 8, $1BF
  dplcEntry 3, $1C7
-DPLC_32c3_27_End
+DPLC_e16e_27_End
 
-DPLC_32c3_28:	dplcHeader
+DPLC_e16e_28:	dplcHeader
  dplcEntry 6, $1B4
  dplcEntry 2, $1CA
  dplcEntry 9, $1CC
  dplcEntry 2, $1D5
-DPLC_32c3_28_End
+DPLC_e16e_28_End
 
-DPLC_32c3_29:	dplcHeader
+DPLC_e16e_29:	dplcHeader
  dplcEntry 6, $17C
  dplcEntry 9, $1D7
  dplcEntry 2, $196
  dplcEntry 2, $1E0
  dplcEntry 1, $18F
-DPLC_32c3_29_End
+DPLC_e16e_29_End
 
-DPLC_32c3_30:	dplcHeader
+DPLC_e16e_30:	dplcHeader
  dplcEntry $10, $1E2
  dplcEntry 9, $1F2
-DPLC_32c3_30_End
+DPLC_e16e_30_End
 
-DPLC_32c3_31:	dplcHeader
+DPLC_e16e_31:	dplcHeader
  dplcEntry $10, $1FB
  dplcEntry 8, $20B
-DPLC_32c3_31_End
+DPLC_e16e_31_End
 
-DPLC_32c3_32:	dplcHeader
+DPLC_e16e_32:	dplcHeader
  dplcEntry $10, $213
  dplcEntry 8, $223
-DPLC_32c3_32_End
+DPLC_e16e_32_End
 
-DPLC_32c3_33:	dplcHeader
+DPLC_e16e_33:	dplcHeader
  dplcEntry $10, $22B
  dplcEntry $A, $23B
-DPLC_32c3_33_End
+DPLC_e16e_33_End
 
-DPLC_32c3_34:	dplcHeader
+DPLC_e16e_34:	dplcHeader
  dplcEntry 6, $245
  dplcEntry 2, $24B
  dplcEntry $C, $24D
  dplcEntry 1, $259
-DPLC_32c3_34_End
+DPLC_e16e_34_End
 
-DPLC_32c3_35:	dplcHeader
+DPLC_e16e_35:	dplcHeader
  dplcEntry 6, $25A
  dplcEntry 2, $260
  dplcEntry $C, $262
-DPLC_32c3_35_End
+DPLC_e16e_35_End
 
-DPLC_32c3_36:	dplcHeader
+DPLC_e16e_36:	dplcHeader
  dplcEntry 6, $245
  dplcEntry 2, $26E
  dplcEntry $C, $270
  dplcEntry 1, $259
-DPLC_32c3_36_End
+DPLC_e16e_36_End
 
-DPLC_32c3_37:	dplcHeader
+DPLC_e16e_37:	dplcHeader
  dplcEntry 6, $25A
  dplcEntry 2, $260
  dplcEntry $C, $27C
-DPLC_32c3_37_End
+DPLC_e16e_37_End
 
-DPLC_32c3_38:	dplcHeader
+DPLC_e16e_38:	dplcHeader
  dplcEntry 6, $288
  dplcEntry $C, $28E
-DPLC_32c3_38_End
+DPLC_e16e_38_End
 
-DPLC_32c3_39:	dplcHeader
+DPLC_e16e_39:	dplcHeader
  dplcEntry 6, $14A
  dplcEntry $C, $29A
-DPLC_32c3_39_End
+DPLC_e16e_39_End
 
-DPLC_32c3_40:	dplcHeader
+DPLC_e16e_40:	dplcHeader
  dplcEntry 6, $288
  dplcEntry $C, $2A6
-DPLC_32c3_40_End
+DPLC_e16e_40_End
 
-DPLC_32c3_41:	dplcHeader
+DPLC_e16e_41:	dplcHeader
  dplcEntry 6, $14A
  dplcEntry $C, $2B2
-DPLC_32c3_41_End
+DPLC_e16e_41_End
 
-DPLC_32c3_42:	dplcHeader
+DPLC_e16e_42:	dplcHeader
  dplcEntry 6, $2BE
  dplcEntry 2, $2C4
  dplcEntry $C, $2C6
  dplcEntry 1, $2D2
-DPLC_32c3_42_End
+DPLC_e16e_42_End
 
-DPLC_32c3_43:	dplcHeader
+DPLC_e16e_43:	dplcHeader
  dplcEntry 8, $2D3
  dplcEntry $C, $2DB
-DPLC_32c3_43_End
+DPLC_e16e_43_End
 
-DPLC_32c3_44:	dplcHeader
+DPLC_e16e_44:	dplcHeader
  dplcEntry 6, $2BE
  dplcEntry 2, $2E7
  dplcEntry $C, $2E9
  dplcEntry 1, $2D2
-DPLC_32c3_44_End
+DPLC_e16e_44_End
 
-DPLC_32c3_45:	dplcHeader
+DPLC_e16e_45:	dplcHeader
  dplcEntry 8, $2D3
  dplcEntry $C, $2F5
-DPLC_32c3_45_End
+DPLC_e16e_45_End
 
-DPLC_32c3_46:	dplcHeader
+DPLC_e16e_46:	dplcHeader
  dplcEntry $10, $301
-DPLC_32c3_46_End
+DPLC_e16e_46_End
 
-DPLC_32c3_47:	dplcHeader
+DPLC_e16e_47:	dplcHeader
  dplcEntry $10, $311
-DPLC_32c3_47_End
+DPLC_e16e_47_End
 
-DPLC_32c3_48:	dplcHeader
+DPLC_e16e_48:	dplcHeader
  dplcEntry $10, $321
-DPLC_32c3_48_End
+DPLC_e16e_48_End
 
-DPLC_32c3_49:	dplcHeader
+DPLC_e16e_49:	dplcHeader
  dplcEntry $10, $331
-DPLC_32c3_49_End
+DPLC_e16e_49_End
 
-DPLC_32c3_50:	dplcHeader
+DPLC_e16e_50:	dplcHeader
  dplcEntry $10, $341
-DPLC_32c3_50_End
+DPLC_e16e_50_End
 
-DPLC_32c3_51:	dplcHeader
+DPLC_e16e_51:	dplcHeader
  dplcEntry $C, $351
  dplcEntry 3, $35D
-DPLC_32c3_51_End
+DPLC_e16e_51_End
 
-DPLC_32c3_52:	dplcHeader
+DPLC_e16e_52:	dplcHeader
  dplcEntry $10, $360
-DPLC_32c3_52_End
+DPLC_e16e_52_End
 
-DPLC_32c3_53:	dplcHeader
+DPLC_e16e_53:	dplcHeader
  dplcEntry $C, $370
  dplcEntry 3, $37C
-DPLC_32c3_53_End
+DPLC_e16e_53_End
 
-DPLC_32c3_54:	dplcHeader
+DPLC_e16e_54:	dplcHeader
  dplcEntry $10, $37F
-DPLC_32c3_54_End
+DPLC_e16e_54_End
 
-DPLC_32c3_55:	dplcHeader
+DPLC_e16e_55:	dplcHeader
  dplcEntry 6, $38F
  dplcEntry $C, $395
-DPLC_32c3_55_End
+DPLC_e16e_55_End
 
-DPLC_32c3_56:	dplcHeader
+DPLC_e16e_56:	dplcHeader
  dplcEntry 6, $3A1
  dplcEntry 8, $3A7
  dplcEntry 2, $3AF
  dplcEntry 1, $3B1
-DPLC_32c3_56_End
+DPLC_e16e_56_End
 
-DPLC_32c3_57:	dplcHeader
+DPLC_e16e_57:	dplcHeader
  dplcEntry $F, $3B2
-DPLC_32c3_57_End
+DPLC_e16e_57_End
 
-DPLC_32c3_58:	dplcHeader
+DPLC_e16e_58:	dplcHeader
  dplcEntry 3, $3C1
  dplcEntry 3, $3C4
  dplcEntry $10, $3C7
-DPLC_32c3_58_End
+DPLC_e16e_58_End
 
-DPLC_32c3_59:	dplcHeader
+DPLC_e16e_59:	dplcHeader
  dplcEntry $C, $3D7
  dplcEntry 8, $3E3
  dplcEntry 1, $A9
-DPLC_32c3_59_End
+DPLC_e16e_59_End
 
-DPLC_32c3_60:	dplcHeader
+DPLC_e16e_60:	dplcHeader
  dplcEntry 8, $3EB
  dplcEntry 4, $3F3
  dplcEntry 3, $3F7
-DPLC_32c3_60_End
+DPLC_e16e_60_End
 
-DPLC_32c3_61:	dplcHeader
+DPLC_e16e_61:	dplcHeader
  dplcEntry 9, $3FA
-DPLC_32c3_61_End
+DPLC_e16e_61_End
 
-DPLC_32c3_62:	dplcHeader
+DPLC_e16e_62:	dplcHeader
  dplcEntry 8, $403
  dplcEntry 1, $40B
  dplcEntry 4, $40C
-DPLC_32c3_62_End
+DPLC_e16e_62_End
 
-DPLC_32c3_63:	dplcHeader
+DPLC_e16e_63:	dplcHeader
  dplcEntry 8, $410
  dplcEntry 4, $418
  dplcEntry 3, $41C
-DPLC_32c3_63_End
+DPLC_e16e_63_End
 
-DPLC_32c3_64:	dplcHeader
+DPLC_e16e_64:	dplcHeader
  dplcEntry $10, $41F
  dplcEntry 5, $42F
-DPLC_32c3_64_End
+DPLC_e16e_64_End
 
-DPLC_32c3_65:	dplcHeader
+DPLC_e16e_65:	dplcHeader
  dplcEntry $C, $434
  dplcEntry 4, $440
  dplcEntry 1, $444
  dplcEntry 1, $445
-DPLC_32c3_65_End
+DPLC_e16e_65_End
 
-DPLC_32c3_66:	dplcHeader
+DPLC_e16e_66:	dplcHeader
  dplcEntry $C, $446
  dplcEntry 4, $452
  dplcEntry 1, $456
  dplcEntry 1, $457
-DPLC_32c3_66_End
+DPLC_e16e_66_End
 
-DPLC_32c3_67:	dplcHeader
+DPLC_e16e_67:	dplcHeader
  dplcEntry 9, $458
  dplcEntry 2, $461
  dplcEntry 6, $463
  dplcEntry 2, $469
  dplcEntry 1, $46B
-DPLC_32c3_67_End
+DPLC_e16e_67_End
 
-DPLC_32c3_68:	dplcHeader
+DPLC_e16e_68:	dplcHeader
  dplcEntry 9, $46C
  dplcEntry 2, $475
  dplcEntry 6, $463
  dplcEntry 2, $469
  dplcEntry 1, $46B
-DPLC_32c3_68_End
+DPLC_e16e_68_End
 
-DPLC_32c3_69:	dplcHeader
+DPLC_e16e_69:	dplcHeader
  dplcEntry 9, $477
  dplcEntry 8, $480
-DPLC_32c3_69_End
+DPLC_e16e_69_End
 
-DPLC_32c3_70:	dplcHeader
+DPLC_e16e_70:	dplcHeader
  dplcEntry 9, $488
  dplcEntry 3, $491
  dplcEntry 2, $494
-DPLC_32c3_70_End
+DPLC_e16e_70_End
 
-DPLC_32c3_71:	dplcHeader
+DPLC_e16e_71:	dplcHeader
  dplcEntry 9, $496
  dplcEntry 8, $49F
-DPLC_32c3_71_End
+DPLC_e16e_71_End
 
-DPLC_32c3_72:	dplcHeader
+DPLC_e16e_72:	dplcHeader
  dplcEntry 9, $488
  dplcEntry 3, $4A7
  dplcEntry 2, $4AA
-DPLC_32c3_72_End
+DPLC_e16e_72_End
 
-DPLC_32c3_73:	dplcHeader
+DPLC_e16e_73:	dplcHeader
  dplcEntry 6, $4AC
  dplcEntry $C, $4B2
-DPLC_32c3_73_End
+DPLC_e16e_73_End
 
-DPLC_32c3_74:	dplcHeader
+DPLC_e16e_74:	dplcHeader
  dplcEntry 9, $4BE
  dplcEntry 4, $4C7
  dplcEntry 1, $4CB
-DPLC_32c3_74_End
+DPLC_e16e_74_End
 
-DPLC_32c3_75:	dplcHeader
+DPLC_e16e_75:	dplcHeader
  dplcEntry 8, $4CC
  dplcEntry 2, $4D4
  dplcEntry $C, $4D6
-DPLC_32c3_75_End
+DPLC_e16e_75_End
 
-DPLC_32c3_76:	dplcHeader
+DPLC_e16e_76:	dplcHeader
  dplcEntry 8, $4E2
  dplcEntry 2, $4EA
  dplcEntry 6, $4EC
  dplcEntry 4, $4F2
  dplcEntry 1, $4F6
-DPLC_32c3_76_End
+DPLC_e16e_76_End
 
-DPLC_32c3_77:	dplcHeader
+DPLC_e16e_77:	dplcHeader
  dplcEntry 8, $4F7
  dplcEntry 2, $4EA
  dplcEntry 6, $4FF
  dplcEntry 4, $4F2
  dplcEntry 1, $4F6
-DPLC_32c3_77_End
+DPLC_e16e_77_End
 
-DPLC_32c3_78:	dplcHeader
+DPLC_e16e_78:	dplcHeader
  dplcEntry 3, $505
  dplcEntry $10, $508
-DPLC_32c3_78_End
+DPLC_e16e_78_End
 
-DPLC_32c3_79:	dplcHeader
+DPLC_e16e_79:	dplcHeader
  dplcEntry 3, $518
  dplcEntry $C, $51B
  dplcEntry 3, $527
-DPLC_32c3_79_End
+DPLC_e16e_79_End
 
-DPLC_32c3_80:	dplcHeader
+DPLC_e16e_80:	dplcHeader
  dplcEntry $C, $52A
-DPLC_32c3_80_End
+DPLC_e16e_80_End
 
-DPLC_32c3_81:	dplcHeader
+DPLC_e16e_81:	dplcHeader
  dplcEntry 6, $536
-DPLC_32c3_81_End
+DPLC_e16e_81_End
 
-DPLC_32c3_82:	dplcHeader
+DPLC_e16e_82:	dplcHeader
  dplcEntry 2, $53C
-DPLC_32c3_82_End
+DPLC_e16e_82_End
 
-DPLC_32c3_83:	dplcHeader
+DPLC_e16e_83:	dplcHeader
  dplcEntry 8, $3EB
  dplcEntry 4, $3F3
  dplcEntry 3, $3F7
-DPLC_32c3_83_End
+DPLC_e16e_83_End
 
-DPLC_32c3_84:	dplcHeader
+DPLC_e16e_84:	dplcHeader
  dplcEntry 8, $403
  dplcEntry 1, $40B
  dplcEntry 4, $40C
-DPLC_32c3_84_End
+DPLC_e16e_84_End
 
-DPLC_32c3_85:	dplcHeader
+DPLC_e16e_85:	dplcHeader
  dplcEntry $C, $53E
  dplcEntry 2, $54A
  dplcEntry 4, $54C
-DPLC_32c3_85_End
+DPLC_e16e_85_End
 
-DPLC_32c3_86:	dplcHeader
+DPLC_e16e_86:	dplcHeader
  dplcEntry 6, $550
  dplcEntry $C, $556
  dplcEntry 2, $A5
-DPLC_32c3_86_End
+DPLC_e16e_86_End
 
-DPLC_32c3_87:	dplcHeader
+DPLC_e16e_87:	dplcHeader
  dplcEntry $10, $562
  dplcEntry 3, $572
-DPLC_32c3_87_End
+DPLC_e16e_87_End
 
 	even
